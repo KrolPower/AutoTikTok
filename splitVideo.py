971cc96974a4e9f8051a6bf17f31ad05d0656c88
KrolPower <osborne_chris@icloud.com>
2022-09-13 18:12:33 -0700

    clip creation and naming works

--- splitVideo.py
+++ splitVideo.py
@@ -14,7 +14,53 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+def split(movieName, clip, clipStart, totalClips, clipEnd):
 
+    # getting only first 5 seconds
+    clip = clip.subclip(clipStart, clipEnd)
+
+
+    # add movie title and part
+
+    # Generate a text clip. You can customize the font, color, etc.
+    txt_clip = TextClip("Part_" + str(totalClips), fontsize=70, color='white')
+
+
+    # Say that you want it to appear 10s at the center of the screen
+    txt_clip = txt_clip.set_pos('top').set_duration(10)  # --------------------------------- make this be the duration of the clip
+
+
+    # Overlay the text clip on the first video clip
+    video = CompositeVideoClip([clip, txt_clip])
+
+
+    # Make New Directory and write clips to it
+
+    # importing os module  
+    import os 
+        
+    # Directory 
+    directory = str(movieName)
+        
+    # Parent Directory path 
+    parent_dir = "D:/AutoTikTok/AutoTikTok/Output"
+        
+    # Path 
+    path = os.path.join(parent_dir, directory) 
+
+    # create directory
+    try: 
+        os.mkdir(path)
+        print(bcolors.OKGREEN + "Directory '" + movieName + "' Created" + bcolors.ENDC)
+    except OSError as error: 
+        print(bcolors.WARNING + str(error) + bcolors.ENDC)  
+    
+    # change directory to output movie clips
+    os.chdir(path)
+
+    # Write the result to a file (many options available !)
+    video.write_videofile(str(movieName) + "_" + str(totalClips) + ".mp4")
+    print(bcolors.OKGREEN + "Clip " + str(movieName) + "_" + str(totalClips) + " Created in Directory " + str(path) + "\n" + bcolors.ENDC)
 
 
 
@@ -29,88 +75,38 @@ movieName = "Piper"
 # loading video gfg
 clip = VideoFileClip(file)
 
-# # Reduce the audio volume (volume x 0.8)
-# clip = clip.volumex(0.8)
+
+
+
+
 # getting duration of the video
 duration = clip.duration
   
 # printing duration
 print("Duration : " + str(duration))
 
-# get duration of video
-
-# split time into 1 minute clips
-
-# add movie title and part
 
 
+# split time into 60 second clips
+
+totalClips = int(duration//60)
 
 
 clipStart = 0
-clipEnd = clipStart + 10
-
-clipCount = 1
+clipEnd = 60
 
 
+for i in range(totalClips):
+    print("Clip Start: " + str(clipStart))
+    print("Clip End: " + str(clipEnd))
 
+    split(movieName, clip, clipStart, i, clipEnd)
 
-
-
-
-
-
-# getting only first 5 seconds
-clip = clip.subclip(clipStart, clipEnd)
-
-
-# Generate a text clip. You can customize the font, color, etc.
-txt_clip = TextClip("Part_" + str(clipCount), fontsize=70, color='white')
-
-
-
-# Say that you want it to appear 10s at the center of the screen
-txt_clip = txt_clip.set_pos('top').set_duration(10)
-
-
-
-# Overlay the text clip on the first video clip
-video = CompositeVideoClip([clip, txt_clip])
+    if i < totalClips:
+        clipStart += 60
+        clipEnd += 60
 
 
 
 
-
-# Make New Directory
-
-# importing os module  
-import os 
-    
-# Directory 
-directory = str(movieName)
-    
-# Parent Directory path 
-parent_dir = "D:/AutoTikTok/AutoTikTok/Output"
-    
-# Path 
-path = os.path.join(parent_dir, directory) 
-
-# create directory
-try: 
-    os.mkdir(path)
-    print(bcolors.OKGREEN + "Directory '" + movieName + "' Created" + bcolors.ENDC)
-except OSError as error: 
-    print(bcolors.WARNING + str(error) + bcolors.ENDC)  
- 
-
-
-
-
-
-
-# change directory to output movie clips
-os.chdir(path)
-
-# Write the result to a file (many options available !)
-video.write_videofile(str(movieName) + "_" + str(clipCount) + ".mp4")
-print(bcolors.OKGREEN + "Clip " + str(movieName) + "_" + str(clipCount) + " Created in Directory " + str(path) + "\n" + bcolors.ENDC)
 
